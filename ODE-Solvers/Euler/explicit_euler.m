e6456b74156c011c9061dde048919015e4a39d1c
romaissa blh <92750169+Romaissa0@users.noreply.github.com>
2026-08-05 09:33:07 +0100

    Add files via upload

---
+++ ODE-Solvers/Euler/explicit_euler.m
@@ -0,0 +1,10 @@
+function [x, y] = explicit_euler(h, x_range, y0)
+    x = x_range(1):h:x_range(2); % Create x points from x_range with step size h
+    y = zeros(size(x));          % Initialize the solution array
+    y(1) = y0;                   % Set initial condition
+    
+    for n = 1:length(x)-1
+        y(n+1) = y(n) + h * exp(-x(n)^2); % Euler's explicit method
+    end
+end
+
